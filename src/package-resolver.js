15eab9f28b034bfec7098c66fc64090e2a8b2dad
Justin Moore <jmoore@mm-mac-0894.local>
2016-06-14 17:40:22 -0500

    Support local json -- even when not named bower.json

--- src/package-resolver.js
+++ src/package-resolver.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var path = require('path');
 
 var local = require('./local-package-resolver');
@@ -9,8 +10,26 @@ var cacheable = ((cache) => {
       cache[endpoint] = fn(endpoint)
 })({});
 
-module.exports = (endpoint) => {
-  var split = endpoint.split(path.sep);
-  return split[split.length - 1] === 'bower.json' ? local(endpoint) :
+var resolution = (endpoint, terminal) => {
+  return terminal === 'bower.json' ? local(endpoint) :
     cacheable(remote)(endpoint);
 };
+
+module.exports = (endpoint) => {
+  var split = endpoint.split(path.sep);
+  var terminal = split[split.length - 1];
+  if (split[0].indexOf(".") === 0) { // endpoint is a relative filepath
+    return new Promise((resolve, reject) => {
+      fs.lstat(endpoint, (err, stats) => {
+        if (err || stats.isDirectory()) {
+          return resolution(endpoint, terminal).then(resolve, reject);
+        } else {
+          return local(endpoint).then(resolve, reject);
+        }
+      });
+    });
+  } else {
+    return resolution(endpoint, terminal);
+  }
+};
+
